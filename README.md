203807db2814dbb8bd68329775f67ca58fec49a7
Eno Compton <ecompton@pivotal.io>
2016-07-26 17:10:12 +0900

    Use less generic sample user, password, and role

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ curl -i http://localhost:8080/
 
 # 200 OK
 # X-AUTH-TOKEN: b912ddc1-e9b5-46f0-8b19-70bad5d21803
-curl -i http://localhost:8080/ -u user:password
+curl -i http://localhost:8080/ -u fancyuser:fancypassword
 
 # 200 OK
 curl -i http://localhost:8080/ -H "X-AUTH-TOKEN: b912ddc1-e9b5-46f0-8b19-70bad5d21803"
--- src/main/java/io/pivotal/SecurityConfig.java
+++ src/main/java/io/pivotal/SecurityConfig.java
@@ -26,6 +26,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
 		auth
 			.inMemoryAuthentication()
-				.withUser("user").password("password").roles("USER");
+				.withUser("fancyuser").password("fancypassword").roles("FANCY_ROLE");
 	}
 }
