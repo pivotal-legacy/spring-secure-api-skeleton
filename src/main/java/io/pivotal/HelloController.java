659de4a54bc31632314db4e55370b5d6f4807a6a
Eno Compton <ecompton@pivotal.io>
2016-07-26 17:19:38 +0900

    Add example of grabbing current user

--- src/main/java/io/pivotal/HelloController.java
+++ src/main/java/io/pivotal/HelloController.java
@@ -1,5 +1,6 @@
 package io.pivotal;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,7 +10,10 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET;
 public class HelloController {
 
     @RequestMapping(value = "/", method = GET)
-    public String getMessage() {
+    public String getMessage(Authentication authentication) {
+        // Here's how you grab the current user.
+        System.out.println("authentication = " + authentication.getPrincipal());
+
         return "hello";
     }
 }
